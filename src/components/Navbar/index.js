bb8231a24baba59579c0088abbd3b850e327d097
pavanvinjamara <pavanvinjamara323@gmail.com>
2024-03-01 01:05:59 +0530

    photos update of navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,36 @@
 import React from "react";
-import SFarmLogo from "./assets/sfarmlogo.png";
-import WishList from "./assets/love.png";
-import ShoppingBag from "./assets/bag.png";
-// import SearchIcon from "./assets/search.png";
+import { useState } from 'react';
 
+// Images
+
+import SframLogo from './assets/sfarmlogo.png'
+import WishList from './assets/heart1.png'
+import ShoppingBag from './assets/shopping-bag.png'
 // Components
 import Hamburger from "../hamburger";
 
 function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const handleHamburger = () => {
+      setActive(!active)
+  };
   return (
-    <div className="w-full h-10 flex justify-between items-center">
-      <div className="w-fit h-full flex">
-        <img src={SFarmLogo} className="w-10 h-10" alt="comapny sframlogo" />
-      </div>
-      <div className="w-fit h-10 flex items-center justify-center">
-        {/* <img src={SearchIcon} className="w-6 h-6" alt="comapny sframlogo" /> */}
-        <img src={WishList} className="w-6 h-6 flex" alt="comapny sframlogo" />
-        <img src={ShoppingBag} className="w-6 h-6" alt="comapny sframlogo" />
-      </div>
+    <div className='w-full h-fit flex justify-between items-center py-2.5 px-4'>
+    <div className='w-fit h-full flex items-center'>
+        <div className="w-fit h-fit flex flex-col cursor-pointer" onClick={handleHamburger}>
+            <span className={`w-6 h-0.5 mb-1 rounded-md bg-[#15864c] transform duration-100 ease-linear ${active ? "rotate-45 absoulte translate-y-1.5" : "rotate-0"}`}></span>
+            <span className={`w-6 h-0.5 mb-1 rounded-md bg-[#15864c] transition-all duration-100 ease-linear ${active ? "-translate-x-full opacity-0 " : "translate-x-0 opacity-100"}`}></span>
+            <span className={`w-6 h-0.5 rounded-md bg-[#15864c] transform duration-100 ease-linear ${active ? "-rotate-45 absoulte -translate-y-1.5" : "rotate-0"}`}></span>
+        </div>
+        <img src={SframLogo} alt="logo" className='w-10 h-10 ml-2'/>
     </div>
+    <div className='w-fit h-full flex items-center'>
+        <div></div>
+        <img src={WishList} alt="logo" className='w-10 h-10 pt-1.5 px-1.5 pb-1'/>
+        <img src={ShoppingBag} alt="logo" className='w-10 h-10'/>
+    </div>
+</div>
   );
 }
 

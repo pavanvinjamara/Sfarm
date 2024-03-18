8bd56f64debd215d2295e50b959d294dba97ff78
pavanvinjamara <pavanvinjamara323@gmail.com>
2024-03-19 00:45:40 +0530

    added searchlist product files and search icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,18 +18,15 @@ function Navbar() {
   return (
     <div className="w-full h-fit flex justify-between items-center py-2.5 px-4">
       <div className="w-fit h-full flex items-center">
-        {/* <div className="w-fit h-fit flex flex-col cursor-pointer" onClick={handleHamburger}>
-                    <span className={`w-6 h-0.5 mb-1 rounded-md bg-[#15864c] transform duration-100 ease-linear ${active ? "rotate-45 absoulte translate-y-1.5" : "rotate-0"}`}></span>
-                    <span className={`w-6 h-0.5 mb-1 rounded-md bg-[#15864c] transition-all duration-100 ease-linear ${active ? "-translate-x-full opacity-0 " : "translate-x-0 opacity-100"}`}></span>
-                    <span className={`w-6 h-0.5 rounded-md bg-[#15864c] transform duration-100 ease-linear ${active ? "-rotate-45 absoulte -translate-y-1.5" : "rotate-0"}`}></span>
-                </div> */}
         <div
-          className="w-fit h-fit flex flex-col cursor-pointer"
+          className="w-fit h-fit flex flex-col relative cursor-pointer"
           onClick={handleHamburger}
         >
           <span
             className={`h-0.5 mb-1 rounded-md bg-[#15864c] transform duration-100 ease-linear ${
-              active ? "-rotate-45 absoulte w-2 translate-y-1" : "w-6 rotate-0"
+              active
+                ? "-rotate-45 absoulte left-0 w-2 translate-y-1"
+                : "w-6 rotate-0"
             }`}
           ></span>
           <span
@@ -37,19 +34,25 @@ function Navbar() {
           ></span>
           <span
             className={`h-0.5 rounded-md bg-[#15864c] transform duration-100 ease-linear ${
-              active ? "rotate-45 absoulte w-2 -translate-y-1" : "w-6 rotate-0"
+              active
+                ? "rotate-45 absoulte left-0 w-2 -translate-y-1"
+                : "w-6 rotate-0"
             }`}
           ></span>
         </div>
         <img src={SframLogo} alt="logo" className="w-10 h-10 ml-2" />
       </div>
       <div className="w-fit h-full flex items-center">
-        <div></div>
-        <img
-          src={WishList}
-          alt="logo"
-          className="w-10 h-10 pt-1.5 px-1.5 pb-1"
-        />
+        <div className="w-full h-full relative p-1">
+          <div className="w-5 h-5 rounded-full border border-px border-[#15864c]"></div>
+          <hr className="w-px h-2.5 absoulte bottom-0 right-0 -rotate-45 bg-[#15864c]"></hr>
+          {/* <hr className="w-px h-2.5 absoulte bottom-0 right-0 rotate-45 bg-[#15864c]"></hr> */}
+        </div>
+        {/* <img
+					src={WishList}
+					alt="logo"
+					className="w-10 h-10 pt-1.5 px-1.5 pb-1"
+					/> */}
         <img src={ShoppingBag} alt="logo" className="w-10 h-10" />
       </div>
     </div>
---
+++ src/pages/SearchList/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SearchList() {
+  return (
+    <div>SearchList</div>
+  )
+}
+
+export default SearchList

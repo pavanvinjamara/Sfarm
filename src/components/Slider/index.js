ec41fa5b9150806df7946800b3afead311676f82
Snehal Dnyaneshwar Khawshi <103592067+snehalkhawshi@users.noreply.github.com>
2024-03-18 23:34:48 +0530

    slider component added

---
+++ src/components/Slider/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from 'react';
+import "./index.scss";
+
+const Slider = () => {
+    return  <div >
+            <h1>Slider component</h1>   
+            </div>
+}
+
+export default Slider;

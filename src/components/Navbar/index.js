59a26977642668f12d5989dbf588d3e1ecb14cc6
pavanvinjamara <pavanvinjamara323@gmail.com>
2024-03-20 01:32:26 +0530

    searchbar in nav bar updated

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,12 +11,17 @@ import Hamburger from "../hamburger";
 
 function Navbar() {
   const [active, setActive] = useState(false);
+  const [searchActive, setSearchActive] = useState(false);
 
   const handleHamburger = () => {
     setActive(!active);
   };
+
+  const handleSearch = () => {
+    setSearchActive(true);
+  };
   return (
-    <div className="w-full h-fit flex justify-between items-center py-2.5 px-4">
+    <div className="w-full h-30 flex justify-between items-center py-2.5 px-4">
       <div className="w-fit h-full flex items-center">
         <div
           className="w-fit h-fit flex flex-col relative cursor-pointer"
@@ -42,10 +47,31 @@ function Navbar() {
         </div>
         <img src={SframLogo} alt="logo" className="w-10 h-10 ml-2" />
       </div>
-      <div className="w-fit h-full flex items-center">
-        <div className="w-full h-full relative p-1">
-          <div className="w-5 h-5 rounded-full border border-px border-[#15864c]"></div>
-          <hr className="w-px h-2.5 absoulte bottom-0 right-0 -rotate-45 bg-[#15864c]"></hr>
+      <div className="w-full h-full flex items-center">
+        <div
+          className="w-full h-full flex justify-end relative p-1"
+          onClick={handleSearch}
+        >
+          <div
+            className={`border border-[1px] border-[#15864c] transition-all duration-100 ease-linear ${
+              searchActive
+                ? "w-full h-6 rounded-md flex grow"
+                : "w-5 h-5 rounded-full"
+            }`}
+          >
+            {searchActive ? (
+              <input
+                type="search"
+                name="search"
+                className="bg-transparent w-full border-0 px-1 py-px outline-0"
+              />
+            ) : (
+              ""
+            )}
+          </div>
+          <hr
+            className={`w-[1.5px] h-2.5 absolute bottom-0 right-1 -rotate-45 bg-[#15864c]`}
+          ></hr>
           {/* <hr className="w-px h-2.5 absoulte bottom-0 right-0 rotate-45 bg-[#15864c]"></hr> */}
         </div>
         {/* <img
